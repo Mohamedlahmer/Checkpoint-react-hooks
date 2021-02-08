4c4e701094c5556a2486f1476c9300bf4ad6ec1c
mohamed lahmer <mohamedlahmer600@gmail.com>
2021-02-08 20:38:13 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import MoviesList from './Component/MoviesList'
+import {Navbar, Nav,Form, FormControl,Button} from "react-bootstrap"
+import React from 'react'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+       <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+  </header>
+  <main>
+
+    <MoviesList/>
+  </main>
+  <footer>it's footer part</footer>
     </div>
   );
 }
---
+++ src/Component/MovieCard/MovieCard.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// const Movie = ({moviesData}) => {
+//     return (
+     
+//     )
+// }
+
+// export default Movie
