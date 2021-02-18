00abf077a6fabc3442ef6ae6028c389591393300
mohamed lahmer <mohamedlahmer600@gmail.com>
2021-02-19 00:08:48 +0100

    final version

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
-import NavBar from './Component/Navbar/Navbar';
-import Footer from './Component/Footer/Footer';
-import MoviesList from './Component/MovieList/MoviesList';
-import AddMovie from './Component/Addmovie/AddMovie'
-import React, { useState } from 'react'
-import './App.css';
-
+import NavBar from "./Component/Navbar/Navbar";
+import Footer from "./Component/Footer/Footer";
+import MoviesList from "./Component/MovieList/MoviesList";
+import AddMovie from "./Component/Addmovie/AddMovie";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [Input, setInput] = useState("");
+  const handledata = (Input) => {
+    setInput(Input);
+  };
 
-const [Input, setInput] = useState("")
-const handledata = (Input) =>{
-  setInput(Input)
-}
+  const [value, setValue] = useState(2);
+  const handlevalue = (value) => {
+    setValue(value);
+  };
 
-const [value, setValue] = useState(2)
-const handlevalue = (value) => {
-  setValue(value)
-}
+  const [DataMovie, setDataMovie] = useState({});
 
-const [DataMovie, setDataMovie] = useState({})
-
-const handleDataMovie = (DataMovie) =>{
-  setDataMovie(DataMovie)
-}
-
-
-
+  const handleDataMovie = (DataMovie) => {
+    setDataMovie(DataMovie);
+  };
 
   return (
     <div>
-     
-      <NavBar handledata={handledata} handlevalue={handlevalue}/>
-      <MoviesList Input={Input} value={value} DataMovie={DataMovie}/>
-      <AddMovie handleDataMovie={handleDataMovie}/>
-      <Footer/>
+      <NavBar handledata={handledata} handlevalue={handlevalue} />
+      <MoviesList Input={Input} value={value} DataMovie={DataMovie} />
+      <AddMovie handleDataMovie={handleDataMovie} />
+      <Footer />
     </div>
   );
 }
